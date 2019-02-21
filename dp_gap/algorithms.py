1543d29bfa2bb98dc975844c88d23028fb3f3546
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-02-21 12:14:03 -0500

    Implement adaptive sparse vector.

--- dp_gap/algorithms.py
+++ dp_gap/algorithms.py
@@ -53,3 +53,30 @@ def gap_noisy_max(q, epsilon):
             if noisy_q_i > max - gap:
                 gap = max - noisy_q_i
     return imax, gap
+
+
+def adaptive_sparse_vector(q, threshold, c, epsilon):
+    out = []
+    count = 1
+    i = 0
+    eta = np.random.laplace(scale=2.0 / epsilon)
+    noisy_threshold = threshold + eta
+    while i < len(q) and count < 2 * c - 1:
+        eta_i = np.random.laplace(scale=8 * c / epsilon)
+        noisy_q_i = q[i] + eta_i
+        if noisy_q_i >= noisy_threshold + 24 * np.sqrt(2) * c / epsilon:
+            out.append(True)
+            count += 1
+        else:
+            psi_i = refinelaplace(eta_i, 0, 8.0 * c / epsilon, 4.0 * c / epsilon)
+            print(psi_i)
+            print('refined')
+            noisy_q_i = q[i] + psi_i
+            if noisy_q_i >= noisy_threshold:
+                out.append(True)
+                count += 2
+            else:
+                out.append(False)
+        i += 1
+
+    return out
