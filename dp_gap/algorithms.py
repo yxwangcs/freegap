c10b3c0190a3bf1cfa6e6e40a14b018409133960
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-02-21 12:13:38 -0500

    Fix the SVT algorithms.

--- dp_gap/algorithms.py
+++ dp_gap/algorithms.py
@@ -1,44 +1,49 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from dp_gap.refinelap import refinelaplace
+
 
 
 def sparse_vector_technique(q, threshold, c, epsilon):
     out = []
     count = 0
-    i = 1
-    eta = np.random.laplace(2.0 / epsilon)
+    i = 0
+    eta = np.random.laplace(scale=2.0 / epsilon)
     noisy_threshold = threshold + eta
-    while i <= len(q) and count < c:
-        eta_i = np.random.laplace(4.0 * c / epsilon)
+    while i < len(q) and count < c:
+        eta_i = np.random.laplace(scale=4.0 * c / epsilon)
         noisy_q_i = q[i] + eta_i
         if noisy_q_i >= noisy_threshold:
             out.append(True)
             count += 1
         else:
             out.append(False)
+        i += 1
     return out
 
 
-def sparse_vector_technique(q, threshold, c, epsilon):
+def gap_sparse_vector_technique(q, threshold, c, epsilon):
     out = []
     count = 0
-    i = 1
-    eta = np.random.laplace(2.0 / epsilon)
+    i = 0
+    eta = np.random.laplace(scale=2.0 / epsilon)
     noisy_threshold = threshold + eta
-    while i <= len(q) and count < c:
-        eta_i = np.random.laplace(4.0 * c / epsilon)
+    while i < len(q) and count < c:
+        eta_i = np.random.laplace(scale=4.0 * c / epsilon)
         noisy_q_i = q[i] + eta_i
         if noisy_q_i >= noisy_threshold:
             out.append(noisy_q_i - noisy_threshold)
             count += 1
         else:
             out.append(False)
+        i += 1
     return out
 
 
 def gap_noisy_max(q, epsilon):
     i, imax, max, gap = 1, 1, 0, 0
     while i <= len(q):
-        eta_i = np.random.laplace(2.0 / epsilon)
+        eta_i = np.random.laplace(scale=2.0 / epsilon)
         noisy_q_i = q[i] + eta_i
         if noisy_q_i > max or i == 1:
             imax = i
