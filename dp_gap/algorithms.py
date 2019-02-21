89dbb71620aef24eced9ba3169a47f275cd33fad
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-02-21 12:51:41 -0500

    Add test for refine laplace.

--- dp_gap/algorithms.py
+++ dp_gap/algorithms.py
@@ -80,3 +80,18 @@ def adaptive_sparse_vector(q, threshold, c, epsilon):
         i += 1
 
     return out
+
+
+def test_refine_laplace():
+    loc, scale = 0, 1
+    new_scale = 1 / 1.5
+    s = np.random.laplace(loc, scale=scale, size=10000)
+    s = np.fromiter((refinelaplace(elem, 0, 1.5, 1) for elem in s), dtype=np.float)
+    count, bins, ignored = plt.hist(s, 200, density=True)
+    x = np.arange(-8., 8., .01)
+    pdf = np.exp(-abs(x - loc) / scale) / (2. * scale)
+    new_pdf = pdf = np.exp(-abs(x - loc) / new_scale) / (2. * new_scale)
+    # plt.plot(x, pdf)
+    plt.plot(x, new_pdf)
+    #plt.show()
+    plt.savefig('noise_down.svg')
