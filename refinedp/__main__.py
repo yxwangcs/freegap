507e1f76f2a0b90a56a2bc4b6fe94aa1a09ed73d
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-02-21 18:51:02 -0500

    Plot the refine laplace figures.

--- refinedp/__main__.py
+++ refinedp/__main__.py
@@ -4,18 +4,29 @@ from refinedp.refinelaplace import refinelaplace
 
 
 def test_refine_laplace():
-    loc, scale = 0, 1
-    new_scale = 1 / 1.5
+    loc, scale, refined_scale = 0, 1, 1 / 2.0
     s = np.random.laplace(loc, scale=scale, size=10000)
-    s = np.fromiter((refinelaplace(elem, 0, 1.5, 1) for elem in s), dtype=np.float)
-    count, bins, ignored = plt.hist(s, 200, density=True)
-    x = np.arange(-8., 8., .01)
+    count, bins, ignored = plt.hist(s, 200, density=True, range=(-4., 4))
+    x = np.arange(-4., 4., .01)
     pdf = np.exp(-abs(x - loc) / scale) / (2. * scale)
-    new_pdf = pdf = np.exp(-abs(x - loc) / new_scale) / (2. * new_scale)
-    # plt.plot(x, pdf)
-    plt.plot(x, new_pdf)
-    #plt.show()
-    plt.savefig('noise_down.svg')
+    plt.plot(x, pdf, label='Laplace($\\mu$=0, scale=1)')
+    plt.title('$\mathtt{Laplace}$')
+    axes = plt.gca()
+    axes.set_ylim([0., 1.])
+    plt.legend()
+    plt.savefig('lap.svg')
+    plt.clf()
+
+    # plot refined laplace
+    s = np.fromiter((refinelaplace(elem, 0, 2, 1) for elem in s), dtype=np.float)
+    count, bins, ignored = plt.hist(s, 200, density=True, range=(-4., 4))
+    refined_pdf = np.exp(-abs(x - loc) / refined_scale) / (2. * refined_scale)
+    plt.plot(x, refined_pdf, label='Laplace($\\mu$=0, scale=2)')
+    plt.title('$\mathtt{RefineLap}$(X, 2, 1)')
+    plt.legend()
+    plt.savefig('refinelap.svg')
+
+
 
 
 def main():
