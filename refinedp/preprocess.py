4c6b7d726d8b4171fc96e3b4f62e8d0b6388ccf6
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-02-21 18:48:56 -0500

    Return the histogram as well as itemsets.

--- refinedp/preprocess.py
+++ refinedp/preprocess.py
@@ -5,7 +5,11 @@ def process_frequent_itemsets(in_file, out_file):
     itemsets = []
     with open(in_file, 'r') as in_f:
         for line in in_f.readlines():
-            itemsets.extend(line.split())
-
-    itemsets = np.unique(np.asarray(itemsets), return_counts=True)[1]
-    np.save(out_file, itemsets)
+            for ch in line.split(','):
+                if ch == '\n':
+                    continue
+                else:
+                    itemsets.append(ch)
+    items = np.asarray(itemsets, dtype=np.int)
+    itemsets = np.unique(np.asarray(items), return_counts=True)[1]
+    return items, itemsets
