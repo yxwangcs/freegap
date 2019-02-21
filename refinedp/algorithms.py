2ba7e3431eae175a0ee1645740bbb83a66d05310
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-02-21 18:49:28 -0500

    Reorder the algorithms.

--- refinedp/algorithms.py
+++ refinedp/algorithms.py
@@ -1,9 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import logging
 from refinedp.refinelaplace import refinelaplace
 
 
-def sparse_vector_technique(q, threshold, c, epsilon):
+logger = logging.getLogger(__name__)
+
+
+def sparse_vector(q, threshold, c, epsilon):
     out = []
     count = 0
     i = 0
@@ -21,7 +24,36 @@ def sparse_vector_technique(q, threshold, c, epsilon):
     return out
 
 
-def gap_sparse_vector_technique(q, threshold, c, epsilon):
+def adaptive_sparse_vector(q, threshold, c, epsilon):
+    out = []
+    count = 1
+    refine_count = 0
+    i = 0
+    eta = np.random.laplace(scale=2.0 / epsilon)
+    noisy_threshold = threshold + eta
+    while i < len(q) and count < 2 * c - 1:
+        eta_i = np.random.laplace(scale=8 * c / epsilon)
+        noisy_q_i = q[i] + eta_i
+        #print(noisy_q_i, noisy_threshold + 8.0 * np.sqrt(2) * c / epsilon)
+        if noisy_q_i >= noisy_threshold + 24.0 * np.sqrt(2) * c / epsilon:
+            out.append(True)
+            count += 1
+        else:
+            psi_i = refinelaplace(eta_i, 0, epsilon / (4.0 * c), epsilon / (8.0 * c))
+            refine_count += 1
+            noisy_q_i = q[i] + psi_i
+            if noisy_q_i >= noisy_threshold:
+                out.append(True)
+                count += 2
+            else:
+                out.append(False)
+        i += 1
+    logger.debug('total refined: {}'.format(refine_count))
+
+    return out
+
+
+def gap_sparse_vector(q, threshold, c, epsilon):
     out = []
     count = 0
     i = 0
@@ -52,30 +84,3 @@ def gap_noisy_max(q, epsilon):
             if noisy_q_i > max - gap:
                 gap = max - noisy_q_i
     return imax, gap
-
-
-def adaptive_sparse_vector(q, threshold, c, epsilon):
-    out = []
-    count = 1
-    i = 0
-    eta = np.random.laplace(scale=2.0 / epsilon)
-    noisy_threshold = threshold + eta
-    while i < len(q) and count < 2 * c - 1:
-        eta_i = np.random.laplace(scale=8 * c / epsilon)
-        noisy_q_i = q[i] + eta_i
-        if noisy_q_i >= noisy_threshold + 24 * np.sqrt(2) * c / epsilon:
-            out.append(True)
-            count += 1
-        else:
-            psi_i = refinelaplace(eta_i, 0, 8.0 * c / epsilon, 4.0 * c / epsilon)
-            print(psi_i)
-            print('refined')
-            noisy_q_i = q[i] + psi_i
-            if noisy_q_i >= noisy_threshold:
-                out.append(True)
-                count += 2
-            else:
-                out.append(False)
-        i += 1
-
-    return out
