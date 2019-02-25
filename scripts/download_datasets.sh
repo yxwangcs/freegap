ba769ab5bcef7e4dfa71c19244f6f209870ba735
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-02-25 16:17:09 -0500

    Add download for census data.

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -6,3 +6,7 @@ wget http://fimi.uantwerpen.be/data/kosarak.dat -O datasets/kosarak.dat
 
 # download BMS-POS
 wget https://raw.githubusercontent.com/cpearce/HARM/master/datasets/BMS-POS.csv -O datasets/BMS-POS.dat
+
+# download census data
+wget https://www2.census.gov/census_2010/04-Summary_File_1/Pennsylvania/pa2010.sf1.zip -O datasets/pa2010.sf1.zip
+unzip datasets/pa2010.sf1.zip
