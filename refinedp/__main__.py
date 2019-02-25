e4f3f51c36cd3a180b49a380f36af231daa27ff3
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-02-25 15:54:27 -0500

    Remove evaluation method for from __main__

--- refinedp/__main__.py
+++ refinedp/__main__.py
@@ -13,34 +13,6 @@ matplotlib.rcParams['text.latex.preamble'] = '\\usepackage{libertine},\\usepacka
 coloredlogs.install(level='INFO', fmt='%(levelname)s - %(name)s %(message)s')
 
 
-def test_refine_laplace():
-    loc, scale, refined_scale = 0, 1, 1 / 2.0
-    s = np.random.laplace(loc, scale=scale, size=10000)
-    count, bins, ignored = plt.hist(s, 150, density=True, range=(-4., 4))
-    x = np.arange(-4., 4., .01)
-    pdf = np.exp(-abs(x - loc) / scale) / (2. * scale)
-    plt.plot(x, pdf, label='\\huge Laplace($\\mu$=0, scale=1)', linewidth=3)
-    plt.title('\\huge X: Laplace')
-    axes = plt.gca()
-    axes.set_ylim([0., 1.])
-    plt.xticks(fontsize=18)
-    plt.yticks(fontsize=18)
-    plt.legend()
-    plt.savefig('lap.pdf')
-    plt.clf()
-
-    # plot refined laplace
-    s = np.fromiter((refinelaplace(elem, 0, 2, 1) for elem in s), dtype=np.float)
-    count, bins, ignored = plt.hist(s, 150, density=True, range=(-4., 4))
-    refined_pdf = np.exp(-abs(x - loc) / refined_scale) / (2. * refined_scale)
-    plt.plot(x, refined_pdf, label='\\huge Laplace($\\mu$=0, scale={})'.format(refined_scale), linewidth=3)
-    plt.title('\\huge RefineLap (X, 1, 2)', fontsize=30)
-    plt.xticks(fontsize=18)
-    plt.yticks(fontsize=18)
-    plt.legend()
-    plt.savefig('refinelap.pdf')
-
-
 def plot_dataset(data):
     plt.hist(data[1], 30, density=True, histtype='bar', ec='black', range=(0, 30))
     plt.show()
