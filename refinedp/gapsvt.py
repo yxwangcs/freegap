3163d0f7bd57652f8ef07ea614625c3a8ca19851
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-02-26 02:11:11 -0500

    Generalize SVT and use best allocation.

--- refinedp/gapsvt.py
+++ refinedp/gapsvt.py
@@ -29,13 +29,16 @@ def num_sparse_vector(q, threshold, c, epsilon):
 
 
 def gap_sparse_vector(q, threshold, c, epsilon):
+    #epsilon_1, epsilon_2 = epsilon / 2.0, epsilon / 2.0
+    ratio = np.power(2 * c, 2.0 / 3)
+    epsilon_1, epsilon_2 = (1.0 / (1 + ratio)) * epsilon, (ratio / (1 + ratio)) * epsilon
     out = []
     count = 0
     i = 0
-    eta = np.random.laplace(scale=2.0 / epsilon)
+    eta = np.random.laplace(scale=1.0 / epsilon_1)
     noisy_threshold = threshold + eta
     while i < len(q) and count < c:
-        eta_i = np.random.laplace(scale=4.0 * c / epsilon)
+        eta_i = np.random.laplace(scale=2.0 * c/ epsilon_2)
         noisy_q_i = q[i] + eta_i
         if noisy_q_i >= noisy_threshold:
             out.append(noisy_q_i - noisy_threshold)
