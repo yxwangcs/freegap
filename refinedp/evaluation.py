c3a2774bb064ff81ef140f186b456d78ca05dc71
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-03-10 01:52:50 -0500

    Implement the simplified evaluation framework.

--- refinedp/evaluation.py
+++ refinedp/evaluation.py
@@ -4,93 +4,72 @@ import matplotlib.pyplot as plt
 import numpy as np
 from refinedp.preprocess import process_bms_pos, process_kosarak
 
+__all__ = ['process_datasets', 'evaluate']
 
 logger = logging.getLogger(__name__)
 
 
-def evaluate(algorithms, epsilon, kwargs=None,
-             # parameters for test
-             c_array=np.array(range(25, 325, 25)), dataset_folder='datasets/', output_folder='./figures/',
-             # parameters for plotting
-             algorithm_names=None):
-    """ Evaluation framework to evaluate the algorithms which returns estimates of top K queries
-    :param algorithms:
-    :param epsilon:
-    :param kwargs:
-    :param c_array:
-    :param dataset_folder:
-    :param output_folder:
-    :param algorithm_names:
-    :return:
-    """
+def process_datasets(folder):
+    logger.info('Loading datasets')
+    dataset_folder = os.path.abspath(folder)
+    # yield different datasets with their names
+    yield 'BMS-POS', process_bms_pos('{}/BMS-POS.dat'.format(dataset_folder))
+    yield 'kosarak', process_kosarak('{}/kosarak.dat'.format(dataset_folder))
 
+# sorted_data = np.sort(data)[::-1]
+# threshold = (sorted_data[c] + sorted_data[c + 1]) / 2.0
+
+
+def mean_square_error(truth, estimates):
+    return np.sum(np.square(truth - estimates)) / float(len(estimates))
+
+
+def evaluate(algorithms, epsilon, input_data, output_folder='./figures/', kwargs=None, c_array=np.array(range(25, 325, 25)),
+             metrics=(mean_square_error, ), algorithm_names=None):
     if algorithm_names is not None:
-        assert len(algorithm_names) == len(algorithms), \
-            'algorithm_names must contain names for all algorithms'
+        assert len(algorithm_names) == len(algorithms), 'algorithm_names must contain names for all algorithms'
     else:
-        algorithm_names = [algorithm.__name__ for algorithm in algorithms]
+        algorithm_names = [algorithm.__name__.replace('_', ' ').title() for algorithm in algorithms]
+
+    # create the output folder if not exists
+    output_folder = '{}/{}'.format(os.path.abspath(output_folder), algorithm_names[0])
+    os.makedirs(output_folder, exist_ok=True)
+    output_prefix = os.path.abspath(output_folder)
 
     logger.info('Evaluating {}'.format(algorithms[0].__name__))
 
-    # create the output folder if not exists
-    try:
-        os.makedirs(output_folder)
-    except FileExistsError:
-        pass
-    path_prefix = os.path.abspath(output_folder)
+    # unpack the input data
+    dataset_name, dataset = input_data
+    logger.info('Evaluating on {}'.format(dataset_name))
 
-    logger.info('Loading datasets')
-    dataset_folder = os.path.abspath(dataset_folder)
-    datasets = {
-        'BMS-POS': process_bms_pos('{}/BMS-POS.dat'.format(dataset_folder)),
-        'kosarak': process_kosarak('{}/kosarak.dat'.format(dataset_folder))
-    }
+    for metric_func in metrics:
+        metric_name = metric_func.__name__.replace('_', ' ').title()
 
-    c_array = list(range(25, 325, 25))
+        metric_data = [[] for _ in range(len(algorithms))]
+        err_data = [[] for _ in range(len(algorithms))]
+        for algorithm_index, algorithm in enumerate(algorithms):
+            for c in c_array:
+                results = []
+                # run several times and record average and error
+                for _ in range(10):
+                    indices, estimates = algorithm(dataset, epsilon, c, **kwargs)
+                    dataset = np.asarray(dataset)
+                    results.append(metric_func(dataset[indices], estimates))
+                results = np.asarray(results)
 
-    for name, data in datasets.items():
-        logger.info('Evaluating on {}'.format(name))
-        sorted_data = np.sort(data)[::-1]
-        metric_data, err_data = [[], []], [[], []]
-        for c in c_array:
-            threshold = (sorted_data[c] + sorted_data[c + 1]) / 2.0
-
-            results_1, results_2, results_3 = [], [], []
-            for _ in range(10):
-                i1, r1 = test_algorithm(data, threshold, c, epsilon)
-                i2, r2 = naive_estimate_sparse_vector(data, threshold, c, epsilon)
-                # i3, r3 = numerical_estimate_sparse_vector(data, threshold, c, epsilon)
-                data = np.asarray(data)
-                gap_err = np.sum(np.square(data[i1] - r1)) / float(len(r1))
-                lap_err = np.sum(np.square(data[i2] - r2)) / float(len(r2))
-                results_1.append(gap_err)
-                results_2.append(lap_err)
-                # results_3.append(num_err)
-            results_1 = np.transpose(results_1)
-            results_2 = np.transpose(results_2)
-            # results_3 = np.transpose(results_3)
-
-            metric_data[0].append(results_1.mean())
-            err_data[0].append(
-                [results_1.mean() - results_1.min(), results_1.max() - results_1.mean()])
-            metric_data[1].append(results_2.mean())
-            err_data[1].append(
-                [results_2.mean() - results_2.min(), results_2.max() - results_2.mean()])
-            # metric_data[2].append(results_3.mean())
-            # err_data[2].append(
-            # [results_3.mean() - results_3.min(), results_3.max() - results_3.mean()])
+                metric_data[algorithm_index].append(results.mean())
+                err_data[algorithm_index].append([results.mean() - results.min(), results.max() - results.mean()])
 
         # plot and save
-        plt.errorbar(c_array, metric_data[0], yerr=np.transpose(err_data[0]),
-                     label='\\huge {}'.format(test_algorithm.__name__), fmt='-o', markersize=12)
-        plt.errorbar(c_array, metric_data[1], yerr=np.transpose(err_data[1]),
-                     label='\\huge {}'.format(reference_algorithms[0].__name__), fmt='-s', markersize=12)
+        for algorithm_index in range(len(algorithms)):
+            plt.errorbar(c_array, metric_data[algorithm_index], yerr=np.transpose(err_data[algorithm_index]),
+                         label='\\huge {}'.format(algorithm_names[algorithm_index]), markersize=12)
         plt.xticks(fontsize=24)
         plt.yticks(fontsize=24)
         plt.legend()
-        plt.ylabel('{}'.format('Mean Square Error'), fontsize=24)
+        plt.ylabel('{}'.format(metric_name), fontsize=24)
         plt.tight_layout()
-        plt.savefig('{}/{}.pdf'.format(path_prefix, name).replace(' ', '_'))
+        plt.savefig('{}/{}-{}.pdf'.format(output_prefix, dataset_name, metric_name))
         plt.clf()
 
-        logger.info('Figures saved to {}'.format(output_folder))
+    logger.info('Figures saved to {}'.format(output_prefix))
