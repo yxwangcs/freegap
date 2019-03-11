f574976cfd41984d1c1494cbfeb3f481c815d0c2
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-03-10 20:02:23 -0400

    Add vanilla sparse vector specially for comparisons with adaptive svt.

--- refinedp/adaptivesvt.py
+++ refinedp/adaptivesvt.py
@@ -28,6 +28,22 @@ def adaptive_sparse_vector(q, epsilon, c, threshold):
         i += 1
     logger.debug('Total refined: {}'.format(refine_count))
 
-    return out
+    return np.nonzero(out), None
 
 
+def sparse_vector(q, epsilon, c, threshold):
+    out = []
+    count = 0
+    i = 0
+    eta = np.random.laplace(scale=2.0 / epsilon)
+    noisy_threshold = threshold + eta
+    while i < len(q) and count < c:
+        eta_i = np.random.laplace(scale=4.0 * c / epsilon)
+        noisy_q_i = q[i] + eta_i
+        if noisy_q_i >= noisy_threshold:
+            out.append(True)
+            count += 1
+        else:
+            out.append(False)
+        i += 1
+    return np.nonzero(out), None
