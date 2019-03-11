a119d61ac735ab413bb33934ebc10065338e0d15
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-03-10 20:12:53 -0400

    Fall back to 1:1 for privacy budget allocation.

--- refinedp/gapsvt.py
+++ refinedp/gapsvt.py
@@ -29,13 +29,13 @@ def gap_sparse_vector(q, epsilon, c, threshold, allocation=(0.5, 0.5)):
 
 def gap_svt_estimates(q, epsilon, c, threshold, allocation=(0.5, 0.5)):
     # budget allocation for gap svt
-    x, y = 1, np.power(2 * c, 2.0 / 3.0)
-    #x, y = 1, 1
+    #x, y = 1, np.power(2 * c, 2.0 / 3.0)
+    x, y = 1, 1
     gap_x, gap_y = x / (x + y), y / (x + y)
 
     # budget allocation between gap / laplace
-    x, y = np.power((2.0 / (gap_x * gap_x) + 8.0 * c * c / (gap_y * gap_y)), 1.0 / 3), np.power(2 * c * c, 1.0 / 3)
-    #x, y = 1, 1
+    #x, y = np.power((2.0 / (gap_x * gap_x) + 8.0 * c * c / (gap_y * gap_y)), 1.0 / 3), np.power(2 * c * c, 1.0 / 3)
+    x, y = 1, 1
     gap_budget, lap_budget = y / (x + y), x / (x + y)
 
     #variance_gap = 2 * (1.0 / ((epsilon * gap_budget * gap_x) * (epsilon * gap_budget * gap_x))) + \
