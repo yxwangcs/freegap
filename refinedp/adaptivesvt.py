bc0d45f65b22c480dbe8c1ca5586a5bc9bb74dbf
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-03-28 01:06:11 -0400

    Add new adaptive sparse vector.

--- refinedp/adaptivesvt.py
+++ refinedp/adaptivesvt.py
@@ -31,6 +31,31 @@ def adaptive_sparse_vector(q, epsilon, c, threshold):
     return np.nonzero(out)[0], None
 
 
+def new_adaptive_svt(q, epsilon, c, threshold):
+    out = []
+    count = 1
+    refine_count = 0
+    i = 0
+    eta = np.random.laplace(scale=2.0 / epsilon)
+    noisy_threshold = threshold + eta
+    while i < len(q) and count < 2 * c - 1:
+        eta_i = np.random.laplace(scale=8.0 * c / epsilon)
+        xi_i = np.random.laplace(scale=4.0 * c / epsilon)
+        if q[i] + eta_i >= noisy_threshold + 16.0 * np.sqrt(2) * c / epsilon:
+            out.append(True)
+            count += 1
+        elif q[i] + xi_i > noisy_threshold:
+            refine_count += 1
+            out.append(True)
+            count += 2
+        else:
+            out.append(False)
+        i += 1
+    logger.debug('Total refined: {}'.format(refine_count))
+
+    return np.nonzero(out)[0], None
+
+
 def sparse_vector(q, epsilon, c, threshold):
     out = []
     count = 0
