9d4d79608de3434ea35cb0f73e5193cf66ddde04
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-03-31 23:28:41 -0400

    Commit process_sf1 method.

--- refinedp/preprocess.py
+++ refinedp/preprocess.py
@@ -1,4 +1,5 @@
 import logging
+import csv
 import numpy as np
 
 
@@ -32,3 +33,16 @@ def process_kosarak(in_file):
 
 def process_t40100k(in_file):
     return _process(in_file)
+
+
+def process_sf1(in_file):
+    with open(in_file, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        data = None
+        for row_index, row in enumerate(reader):
+            if row_index == 1:
+                data = row
+        data = tuple(map(lambda cell: int(cell), data[3:]))
+        logger.info('Statistics for {}:  # of Items: {}'.format(in_file, len(data)))
+        return data
+
