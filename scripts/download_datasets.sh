51327b563ab46337e0fb678c64251c42d7f73b65
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-04-01 11:33:20 -0400

    Remove download for SF1 dataset.

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -7,9 +7,5 @@ wget http://fimi.uantwerpen.be/data/kosarak.dat -O datasets/kosarak.dat
 # download BMS-POS
 wget https://raw.githubusercontent.com/cpearce/HARM/master/datasets/BMS-POS.csv -O datasets/BMS-POS.dat
 
-# download census data
-wget https://www2.census.gov/census_2010/04-Summary_File_1/Pennsylvania/pa2010.sf1.zip -O datasets/pa2010.sf1.zip
-unzip datasets/pa2010.sf1.zip -d datasets/pa2010
-
 # download T40I10D100K
 wget http://fimi.uantwerpen.be/data/T40I10D100K.dat -O datasets/T40I10D100K.dat
