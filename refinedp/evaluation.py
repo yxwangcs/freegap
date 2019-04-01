65de91a9e6b7f7c50757b6a311f8fe30922dd40c
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-03-31 23:00:22 -0400

    Only plot improvement for mean square error.
    Also add multiprocess support.

--- refinedp/evaluation.py
+++ refinedp/evaluation.py
@@ -2,9 +2,10 @@ import logging
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-from refinedp.preprocess import process_bms_pos, process_kosarak
+import multiprocessing as mp
+from functools import partial
+from refinedp.preprocess import process_bms_pos, process_kosarak, process_t40100k
 
-__all__ = ['process_datasets', 'evaluate']
 
 logger = logging.getLogger(__name__)
 
@@ -59,12 +60,49 @@ def f_measure(sorted_indices, c_val, indices, truth_indices, truth_estimates, es
 
 
 def evaluate_c(c, algorithm, epsilon, metrics, dataset, sorted_indices):
+    # for svts
+    kwargs = {}
+    threshold_index = 2 * c if 'adaptive' in algorithm.__name__ else c
+    if 'threshold' in algorithm.__code__.co_varnames:
+        threshold = (dataset[sorted_indices[threshold_index]] + dataset[sorted_indices[threshold_index + 1]]) / 2.0
+        kwargs['threshold'] = threshold
+
+    truth_indices = sorted_indices[:threshold_index]
+
+    # run several times and record average and error
+    results = [[] for _ in range(len(metrics))]
+    for _ in range(2000):
+        indices, estimates = algorithm(dataset, epsilon, c, **kwargs)
+        for metric_index, metric_func in enumerate(metrics):
+            results[metric_index].append(
+                metric_func(sorted_indices, c, indices, truth_indices, dataset[indices], estimates))
+
+    results = np.asarray(results)
+    final_metric_data = []
+    final_error_data = []
+    for metric_index in range(len(metrics)):
+        """
+        metric_data[epsilon][metric_index][algorithm_index].append(results[metric_index].mean())
+        err_data[epsilon][metric_index][algorithm_index].append(
+            (results[metric_index].mean() - results[metric_index].min(),
+             results[metric_index].max() - results[metric_index].mean()))
+        """
+        final_metric_data.append(results[metric_index].mean())
+        final_error_data.append((results[metric_index].mean() - results[metric_index].min(),
+                                 results[metric_index].max() - results[metric_index].mean()))
+
+    return final_metric_data, final_error_data
+
+
+def evaluate(algorithms, epsilons, input_data, output_folder='./figures/', c_array=np.array(range(2, 25)),
              metrics=(mean_square_error, above_threshold_answers, accuracy, normalized_cumulative_rank),
              algorithm_names=None):
     if algorithm_names is not None:
         assert len(algorithm_names) == len(algorithms), 'algorithm_names must contain names for all algorithms'
     else:
         algorithm_names = [algorithm.__name__.replace('_', ' ').title() for algorithm in algorithms]
+    # flatten epsilon
+    epsilons = (epsilons, ) if isinstance(epsilons, (int, float)) else epsilons
 
     # create the output folder if not exists
     output_folder = '{}/{}'.format(os.path.abspath(output_folder), algorithms[0].__name__)
@@ -77,64 +115,52 @@ def evaluate_c(c, algorithm, epsilon, metrics, dataset, sorted_indices):
     sorted_indices = np.argsort(dataset)[::-1]
     logger.info('Evaluating {} on {}'.format(algorithms[0].__name__.replace('_', ' ').title(), dataset_name))
 
-    metric_data = [[[] for _ in range(len(algorithms))] for _ in range(len(metrics))]
-    err_data = [[[] for _ in range(len(algorithms))] for _ in range(len(metrics))]
-    for algorithm_index, algorithm in enumerate(algorithms):
-        for c in c_array:
-            # for svts
-            kwargs = {}
-            threshold_index = 2 * c if 'adaptive' in algorithm.__name__ else c
-            if 'threshold' in algorithm.__code__.co_varnames:
-                threshold = (dataset[sorted_indices[threshold_index]] + dataset[sorted_indices[threshold_index + 1]]) / 2.0
-                kwargs['threshold'] = threshold
-
-            truth_indices = sorted_indices[:threshold_index]
-
-            # run several times and record average and error
-            results = [[] for _ in range(len(metrics))]
-            for _ in range(2000):
-                indices, estimates = algorithm(dataset, epsilon, c, **kwargs)
-                for metric_index, metric_func in enumerate(metrics):
-                    results[metric_index].append(
-                        metric_func(sorted_indices, c, indices, truth_indices, dataset[indices], estimates))
-
-            results = np.asarray(results)
-
-            for metric_index in range(len(metrics)):
-                metric_data[metric_index][algorithm_index].append(results[metric_index].mean())
-                err_data[metric_index][algorithm_index].append(
-                    (results[metric_index].mean() - results[metric_index].min(),
-                     results[metric_index].max() - results[metric_index].mean()))
-
+    with mp.Pool(mp.cpu_count()) as pool:
+        metric_data = {}
+        err_data = {}
+        for epsilon in epsilons:
+            metric_data[epsilon] = [[[] for _ in range(len(algorithms))] for _ in range(len(metrics))]
+            err_data[epsilon] = [[[] for _ in range(len(algorithms))] for _ in range(len(metrics))]
+            for algorithm_index, algorithm in enumerate(algorithms):
+                partial_evaluate_c = partial(evaluate_c,
+                                             algorithm=algorithm, epsilon=epsilon, metrics=metrics, dataset=dataset,
+                                             sorted_indices=sorted_indices)
+                for local_metric_data, local_error_data in pool.map(partial_evaluate_c, c_array):
+                    for metric_index in range(len(metrics)):
+                        metric_data[epsilon][metric_index][algorithm_index].append(local_metric_data[metric_index])
+                        err_data[epsilon][metric_index][algorithm_index].append(local_error_data[metric_index])
     # plot and save
     formats = ['-o', '-s']
+    markers = ['o', 's', '^']
     for metric_index, metric_func in enumerate(metrics):
-        fig = plt.figure()
-        ax1 = fig.add_subplot(111)
-        ax2 = ax1.twinx()
         metric_name = metric_func.__name__.replace('_', ' ').title()
         for algorithm_index in range(len(algorithms)):
-            ax1.errorbar(c_array, metric_data[metric_index][algorithm_index],
-                         #yerr=np.transpose(err_data[metric_index][algorithm_index]),
-                         label='\\huge {}'.format(algorithm_names[algorithm_index]),
-                         fmt=formats[algorithm_index % len(formats)], markersize=12)
-            if algorithm_index != 0 and metric_index == 0:
-                ax2.plot(c_array,
-                         100 * (np.asarray(metric_data[0][0] - np.asarray(metric_data[0][algorithm_index]))) / np.asarray(metric_data[0][0]),
-                         'go--', label='\\huge Improvement')
-                ax2.set_ylim(0, 100)
-                ax2.set_ylabel('\\huge Improvement Percentage %')
-
-        ax1.tick_params(axis='both', which='major', labelsize=18)
+            for epsilon_index, epsilon in enumerate(epsilons):
+                if metric_func == mean_square_error:
+                    if algorithm_index == 0:
+                        continue
+                    plt.plot(c_array,
+                             100 * (np.asarray(metric_data[epsilon][0][0] - np.asarray(metric_data[epsilon][0][algorithm_index]))) / np.asarray(metric_data[epsilon][0][0]),
+                             label='\\huge {}'.format(algorithm_names[algorithm_index]),
+                             marker=markers[epsilon_index % len(markers)], linewidth=3,
+                             markersize=10)
+                    plt.ylim(0, 30)
+                    plt.ylabel('\\huge Mean Square Error Improvement \\%')
+                else:
+                    plt.errorbar(c_array, metric_data[epsilon][metric_index][algorithm_index],
+                                 # yerr=np.transpose(err_data[metric_index][algorithm_index]),
+                                 label='\\huge {}'.format(algorithm_names[algorithm_index]),
+                                 fmt=formats[algorithm_index % len(formats)], linewidth=3, markersize=10)
+                    plt.ylabel('{}'.format(metric_name), fontsize=24)
+        if metric_func == precision:
+            plt.ylim(0.0, 1.0)
+        plt.xticks(fontsize=24)
         plt.yticks(fontsize=24)
-        plt.xlim(c_array.min(), c_array.max())
-        lines, labels = ax1.get_legend_handles_labels()
-        lines2, labels2 = ax2.get_legend_handles_labels()
-        legend = ax1.legend(lines + lines2, labels + labels2)
+        #plt.xlim(c_array.min(), c_array.max())
+        legend = plt.legend()
         legend.get_frame().set_linewidth(0.0)
-        ax1.set_ylabel('{}'.format(metric_name), fontsize=24)
         plt.tight_layout()
-        fig.savefig('{}/{}-{}.pdf'.format(output_prefix, dataset_name, metric_name.replace(' ', '_')))
+        plt.savefig('{}/{}-{}.pdf'.format(output_prefix, dataset_name, metric_name.replace(' ', '_')))
         plt.clf()
 
     logger.info('Figures saved to {}'.format(output_prefix))
