d92555a432c398c8af49b0d4dc016de0b3eb7e59
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-03-31 23:00:32 -0400

    Add log output.

--- refinedp/preprocess.py
+++ refinedp/preprocess.py
@@ -1,16 +1,23 @@
+import logging
 import numpy as np
+
+
+logger = logging.getLogger(__name__)
 
 
 def _process(in_file, delimiter=' '):
     itemsets = []
+    records = 0
     with open(in_file, 'r') as in_f:
         for line in in_f.readlines():
             for ch in line.split(delimiter):
                 if ch == '\n':
+                    records += 1
                     continue
                 else:
                     itemsets.append(ch)
     itemsets = np.unique(np.asarray(np.asarray(itemsets, dtype=np.int)), return_counts=True)
+    logger.info('Statistics for {}: # of records: {} and # of Items: {}'.format(in_file, records + 1, len(itemsets[0])))
     return itemsets[1]
 
 
