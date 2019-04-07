06d41850fe4b64663897b4543560e73a9b1d6e56
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-04-07 00:03:16 -0400

    Output middle and top branch information.

--- refinedp/adaptivesvt.py
+++ refinedp/adaptivesvt.py
@@ -7,6 +7,8 @@ logger = logging.getLogger(__name__)
 
 def adaptive_sparse_vector(q, epsilon, c, threshold):
     out = []
+    top_branch = []
+    middle_branch = []
     count = 1
     refine_count = 0
     i = 0
@@ -17,17 +19,21 @@ def adaptive_sparse_vector(q, epsilon, c, threshold):
         xi_i = np.random.laplace(scale=4.0 * c / epsilon)
         if q[i] + eta_i >= noisy_threshold + 16.0 * np.sqrt(2) * c / epsilon:
             out.append(True)
+            top_branch.append(True)
             count += 1
         elif q[i] + xi_i > noisy_threshold:
             refine_count += 1
             out.append(True)
+            middle_branch.append(True)
             count += 2
         else:
             out.append(False)
+            top_branch.append(False)
+            middle_branch.append(False)
         i += 1
     logger.debug('Total refined: {}'.format(refine_count))
 
-    return np.nonzero(out)[0], None
+    return np.nonzero(out)[0], None, np.nonzero(top_branch)[0], np.nonzero(middle_branch)[0]
 
 
 def sparse_vector(q, epsilon, c, threshold):
@@ -45,4 +51,4 @@ def sparse_vector(q, epsilon, c, threshold):
         else:
             out.append(False)
         i += 1
-    return np.nonzero(out)[0], None
+    return np.nonzero(out)[0], None, np.nonzero(out)[0], np.asarray([])
